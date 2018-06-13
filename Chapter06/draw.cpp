f5914e756144c8b636a2d4fecf34661500d8dfce
FLHonker <frankliuceo@163.com>
2018-06-13 22:41:28 +0800

    putText

--- Chapter06/draw.cpp
+++ Chapter06/draw.cpp
@@ -11,13 +11,18 @@ int main(int argc, char **argv)
 {
     Mat pic0 = imread(argv[1], -1);
     Mat pic1(pic0);
-    Point pt(pic1.rows, pic1.cols);
+    Point2i pt1(pic1.rows/3, pic1.cols/3), pt2(pic1.rows/2, pic1.cols/2);
     Scalar color(0,0,255);
+    Size ellipse_sz(60, 40);
+    String text("你好，opencv3!");
     std::cout << "pic1 size = " << pic1.size << std::endl;
-    std::cout << "point:" << std::endl << pt << std::endl;
+    std::cout << "point:" << std::endl << pt1 << std::endl;
     namedWindow("draw", cv::WINDOW_AUTOSIZE);
     // draw
-    circle(pic1, pt, 60, color, 1, 8, 1.2);
+    circle(pic1, pt1, 60, color, 1, 8, 1.2);
+    ellipse(pic1, pt2, ellipse_sz, 60, 0, 360, color);
+    // 中文乱码，cv字体不支持中文
+    putText(pic1, text, pt1, cv::FONT_HERSHEY_COMPLEX, 1.0, color);
     imshow("draw", pic1);
     waitKey(0);
 
